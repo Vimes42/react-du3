bff6c9ed619b5cfe8c584ba7f030df737fe17162
Lenka Polcerova <lenka.polcerova@gmail.com>
2024-10-21 22:09:23 +0200

    done

--- src/components/Star/index.jsx
+++ src/components/Star/index.jsx
@@ -1,17 +1,16 @@
-import { useState } from 'react';
 import './style.css';
 
-export const Star = ({ value, onSelect }) => {
-  const [glowing, setGlowing] = useState(false);
-  
+export const Star = ({ value, onSelect, glowing }) => {
+
   const starClass = glowing
     ? 'rating__star rating__star--on'
     : 'rating__star';
 
     const handleClick = () => {
-      setGlowing(true);
+      glowing ? !glowing : glowing;
       onSelect(value);
     }
+
   return (
     <div className={starClass} onClick={handleClick} ></div>
   );
